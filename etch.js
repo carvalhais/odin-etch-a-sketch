1c11970285e6e4bbf353485ca65858ba5b62852b
Andre Carvalhais <carvalhais@live.com>
2025-11-07 11:04:28 -0300

    Initial commit

---
+++ etch.js
@@ -0,0 +1,57 @@
+const DEFAULT_GRID_DIMENSION = 16;
+
+const gridContainer = document.querySelector("#grid-container");
+const resetButton = document.querySelector("#reset-button");
+
+function buildGrid(dim) {
+    for (let i = 0; i < dim; i++) {
+        let gridLine = document.createElement("div");
+        gridLine.classList.add("grid-line");
+        for (let j = 0; j < dim; j++) {
+            let gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cell");
+            gridCell.id = `cell-${i}.${j}`;
+            gridLine.appendChild(gridCell)
+        }
+        gridContainer.appendChild(gridLine);
+    }
+}
+
+function hoverHandler(e) {
+    if(e.target === gridContainer) return;
+    let classes = Array.from(e.target.classList);
+    if (!classes.includes("pixel-on")) {
+        e.target.classList.add("pixel-on")
+    }
+}
+
+function promptGridSize() {
+    message = "Please, input the new grid size.\n";
+    message += "Valid values are integers between 16 and 100.";
+    return prompt(message, DEFAULT_GRID_DIMENSION);
+}
+
+function validateUserInput(input) {
+    size = parseInt(input);
+    if (size === NaN || size < 16 || size > 100) {
+        alert("Please, input a valid integer betwee 16 and 100.")
+        return null;
+    }
+    return size;
+}
+
+function resetHandler() {
+    input = promptGridSize();
+    let size = validateUserInput(input);
+    if (size !== null) {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.lastChild);
+        }
+        buildGrid(size);
+    }
+}
+
+gridContainer.addEventListener("mouseover", hoverHandler);
+resetButton.addEventListener("click", resetHandler);
+
+buildGrid(DEFAULT_GRID_DIMENSION);
